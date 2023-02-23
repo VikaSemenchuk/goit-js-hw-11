30ec1ca1e1a0e304b86de57cce6e0e893aeded36
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-02-23 12:07:56 +0100

    onSubmit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 import { fetchImages } from "./js/fetchImages";
 
+const formEl = document.getElementById("search-form");
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-console.log('ok');
+formEl.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const value = form.elements.searchQuery.value.trim();
+
+    fetchImages(value).then(({ hits }) => {
+        if (hits.length === 0) throw new Error ('no data')
+        console.log(hits);
+    })
+    .catch(onError)
+    .finally(() => formEl.reset())
+    console.log(value);
+
+    
+}
+console.log(form);
+
+
+function onError(err) {
+    console.log(err);
+}
+// console.log(galleryEl);
+// console.log(loadMoreBtn);
+
+console.log(fetchImages);
+fetchImages();
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,10 +1,11 @@
-export function fetchImages() {
+export function fetchImages(quary) {
     const ENDPOINT = `https://pixabay.com/api/`;
 
-    return fetch (`${ENDPOINT}?key=33731840-f6299e204f5104584f5709ced&q&image_type=photo&orientation=horizontal&safesearch=true`)
+    return fetch (`${ENDPOINT}?key=33731840-f6299e204f5104584f5709ced&q=${quary}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then(response => {
         if (!response.ok) throw new Error(response.status)
 
         return response.json();
-    });
+    })
+    // .then(({ hits }) => console.log(hits))
 }
